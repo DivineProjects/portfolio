db629a8ab9597a256706ca78382b581c6b842b5c
divineProjects <divinejigu@gmail.com>
2025-10-21 04:26:48 +0200

    update

--- src/config.js
+++ src/config.js
@@ -168,7 +168,7 @@ export const resume = null;
  ************************************************************** 
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
-export const filteredProjects = ["https://market-forge.vercel.app/", "example-2", "example-3"];
+export const filteredProjects = ["market-forge", "NJSCarSales", "example-3"];
 
 // Replace the defualt GitHub image for matching repos below (images imported above - lines 7-8)
 export const projectCardImages = [
